f34396cd7081d1d6b544acbe192137d323d24ae0
Masanori Tani <uta8a@uta8a.org>
2025-04-03 20:35:33 +0900

    fix exports

--- main.ts
+++ main.ts
@@ -1,15 +1,16 @@
 import { program } from "npm:commander@13.1.0";
 import { run } from "./mcp/server.ts";
 
-program
-  .name("linkding-mcp-server")
-  .description("Linkding MCP server")
-  .version("0.1.0");
-program.parse();
-
-try {
-  await run();
-} catch (e) {
-  console.error(e);
-  Deno.exit(1);
+if (import.meta.main) {
+  program
+    .name("linkding-mcp-server")
+    .description("Linkding MCP server")
+    .version("0.1.0");
+  program.parse();
+  try {
+    await run();
+  } catch (e) {
+    console.error(e);
+    Deno.exit(1);
+  }
 }
